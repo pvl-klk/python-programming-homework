ef7aa4afafb31ab0a68f2271f0c62fe970cd6ec3
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2025-10-28 03:40:06 +0300

    Fixed the style in Extended Euclidean algorithm\algorithm.py to pass the ruff check

--- src/Homework #2/Extended Euclidean algorithm/alghoritm.py
+++ src/Homework #2/Extended Euclidean algorithm/alghoritm.py
@@ -1,16 +1,16 @@
 def extended_gcd(a: int, b: int) -> tuple[int, int, int]:
     x_previous, x = 1, 0
     y_previous, y = 0, 1
-    
+
     while b != 0:
         q = a // b
         r = a % b
-        
+
         x_previous, x = x, x_previous - q * x
         y_previous, y = y, y_previous - q * y
-        
+
         a, b = b, r
-    
+
     return a, x_previous, y_previous
 
 
