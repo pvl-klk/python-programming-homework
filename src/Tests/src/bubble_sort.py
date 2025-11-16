d2ee48386b1b63d3d9301d417d308148002e925f
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2025-11-17 01:02:17 +0300

    Small fixes

--- src/Tests/src/bubble_sort.py
+++ src/Tests/src/bubble_sort.py
@@ -3,4 +3,5 @@ def bubble_sort(array: list) -> list:
         for index_2 in range(index_1+1, len(array)):
             if array[index_1] > array[index_2]:
                 array[index_1], array[index_2] = array[index_2], array[index_1]
+                
     return array
