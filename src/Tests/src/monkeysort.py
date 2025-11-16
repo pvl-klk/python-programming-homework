931fe71caebd39418ead7a6c183a160084093c69
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2025-11-16 19:47:51 +0300

    Renamed sorting functions

--- src/Tests/src/monkeysort.py
+++ src/Tests/src/monkeysort.py
@@ -1,6 +1,6 @@
 from random import shuffle
 
-def monekey_sort(array: list) -> list:
+def monkeysort(array: list) -> list:
     while True:
         for index in range(1, len(array)):
             if array[index-1] > array[index]:
